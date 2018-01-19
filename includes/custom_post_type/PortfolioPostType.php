3be5ea7e4d1e56ef3c2d85e26df0a6d6e4e686ea
Vladimir <jul-pro@ukr.net>
2018-01-19 17:58:02 +0200

    taxonomies

--- includes/custom_post_type/PortfolioPostType.php
+++ includes/custom_post_type/PortfolioPostType.php
@@ -6,6 +6,7 @@ class PortfolioPostType {
     public function __construct() {
         error_log( 'plugin ' . MFW_PLUGIN_NAME . ' portfolio' );
         add_action( 'init', array( $this, 'portfolio_cpt' ) );
+        add_action( 'init', array( $this, 'create_portfolio_taxonomies' ) );
         
         add_action( 'pre_get_posts', array( $this, 'portfolio_pagesize') );
     }
@@ -48,7 +49,7 @@ class PortfolioPostType {
                 'thumbnail',
                 'revisions',
             ),
-            'taxonomies' => array('category', 'post_tag'),
+            'taxonomies' => array('type'),
             'menu_position' => 5,
             'exclude_from_search' => false
             
@@ -56,6 +57,39 @@ class PortfolioPostType {
         
         register_post_type( 'portfolio', $args );
 //        register_post_type( 'portfolio');
+    }
+    
+    function create_portfolio_taxonomies() {
+        $labels = array(
+            'name' => 'Types',
+            'singular_name' => 'Type',
+            'search_items' => 'Search Types',
+            'all_items' => 'All Types',
+            'parent_item' => 'Parent Type',
+            'parent_item_colon' => 'Parent Type:',
+            'edit_item' => 'Edit Type',
+            'update_item' => 'Update Type',
+            'add_new_item' => 'Add New Type',
+            'new_item_name' => 'New Type Name',
+            'menu_name' => 'Types',
+        );
+        
+        $args = array(
+            'hierarchical' => true,
+            'labels' => $labels,
+            'show_ui' => true,
+            'show_admin_column' => true,
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'pf-type' ),
+        );
+        
+        register_taxonomy('pf-type', array( 'portfolio' ), $args);
+        
+        register_taxonomy( 'software', 'portfolio', array(
+            'label' => 'Software',
+            'rewrite' => array( 'slug' => 'software' ),
+            'hierarchical' => false
+        ) );
     }
 }
 
